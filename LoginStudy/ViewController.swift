3b20eb1465db141d71716d1673741ecb047606c3
dohyun <thtree523@gmail.com>
2024-04-22 14:08:32 +0900

    Project Init

---
+++ LoginStudy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LoginStudy
+//
+//  Created by 김도현 on 2024/04/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func login(_ sender: Any) {
+        print("login")
+    }
+    @IBAction func logout(_ sender: Any) {
+        print("logout")
+    }
+}
